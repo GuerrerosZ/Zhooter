fabadeb85f61fd227dccb9195f92276f0574941d
Francisco <franciscooberto@gmail.com>
2021-06-11 23:57:34 -0300

    Corrijo detalles de la vista gameover

--- gameOver.php
+++ gameOver.php
@@ -1,2 +1,31 @@
-<h1>Game Over</h1>
-<a href="index.php">Volver al menú principal.</a>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estadisticas</title>
+    <link rel="stylesheet" href="./css/estadisticasYMuerte.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div id="title">
+        <h1>|----------&gt HAS MUERTO &lt----------|</h1>
+    </div>
+    <div id="flex">
+        <div class="infoMuerte">
+            <p id="p1">ENEMIGOS DERROTADOS:</p>
+            <p id="p2">NIVEL ALCANZADO:</p>
+        </div>
+    </div>
+    <div class="pauseContainer">
+        <div>
+            <button id="reinicio" onclick="location.href='index.php'">SALIR AL MENU PRICIPAL</button>
+        </div>
+    </div>
+</body>
+
+</html>
